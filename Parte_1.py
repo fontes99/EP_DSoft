7ebf63d2f0d1cc26c5a0fb363334bd5370e49f32
Pedro Fontes <pvfontes1000@gmail.com>
2018-04-24 14:15:30 -0300

    primeira versão

---
+++ Parte_1.py
@@ -0,0 +1,8 @@
+print('Controle de estoque')
+print('0 - sair')
+print('1 - adicionar item')
+print('2 - remover item')
+print('3 - alterar item')
+print('4 - imprimir estoque')
+
+escolha = int(input('Faça sua escolha: '))
